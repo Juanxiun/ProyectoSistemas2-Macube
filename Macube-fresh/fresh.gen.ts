75a3c748c6eaad7297dfeaa14825aeb68b5d3253
Juanxiun <juanitorex1039@gmaio.com>
2025-03-20 00:42:19 -0400

    actualizacion

--- Macube-fresh/fresh.gen.ts
+++ Macube-fresh/fresh.gen.ts
@@ -4,63 +4,39 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-
-import * as $api_Login from "./routes/api/Login.ts";
 import * as $api_Middleware from "./routes/api/Middleware.ts";
-import * as $index from "./routes/index.tsx";
-import * as $CataContainer from "./islands/CataContainer.tsx";
-import * as $Footer from "./islands/Footer.tsx";
-import * as $NavBar from "./islands/NavBar.tsx";
-import * as $PagInicio_ContMain from "./islands/PagInicio/ContMain.tsx";
-
 import * as $api_cliente from "./routes/api/cliente.ts";
-import * as $api_joke from "./routes/api/joke.ts";
-
-import * as $api_login from "./routes/api/login.ts";
+import * as $api_login from "./routes/api/login.tsx";
+import * as $api_session from "./routes/api/session.ts";
 import * as $dashboard from "./routes/dashboard.tsx";
 import * as $index from "./routes/index.tsx";
 import * as $login from "./routes/login.tsx";
+import * as $CataContainer from "./islands/CataContainer.tsx";
+import * as $Footer from "./islands/Footer.tsx";
 import * as $Login from "./islands/Login.tsx";
-
-import * as $index from "./routes/index.tsx";
-
+import * as $NavBar from "./islands/NavBar.tsx";
+import * as $PagInicio_ContMain from "./islands/PagInicio/ContMain.tsx";
 import type { Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-<<<<<<< HEAD
-    "./routes/api/Login.ts": $api_Login,
     "./routes/api/Middleware.ts": $api_Middleware,
-=======
     "./routes/api/cliente.ts": $api_cliente,
-    "./routes/api/joke.ts": $api_joke,
-<<<<<<< HEAD
-    "./routes/api/login.ts": $api_login,
+    "./routes/api/login.tsx": $api_login,
+    "./routes/api/session.ts": $api_session,
     "./routes/dashboard.tsx": $dashboard,
-=======
->>>>>>> fd381c0cd781c5633e4c5b24d0f9b701db438664
->>>>>>> 90dcc65ec2167b579f78b54b570c9bf9b846b1c4
     "./routes/index.tsx": $index,
     "./routes/login.tsx": $login,
   },
-<<<<<<< HEAD
   islands: {
     "./islands/CataContainer.tsx": $CataContainer,
     "./islands/Footer.tsx": $Footer,
+    "./islands/Login.tsx": $Login,
     "./islands/NavBar.tsx": $NavBar,
     "./islands/PagInicio/ContMain.tsx": $PagInicio_ContMain,
   },
-=======
-<<<<<<< HEAD
-  islands: {
-    "./islands/Login.tsx": $Login,
-  },
-=======
-  islands: {},
->>>>>>> fd381c0cd781c5633e4c5b24d0f9b701db438664
->>>>>>> 90dcc65ec2167b579f78b54b570c9bf9b846b1c4
   baseUrl: import.meta.url,
 } satisfies Manifest;
 
