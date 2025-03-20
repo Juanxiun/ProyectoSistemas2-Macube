1290d71753cc0122601f329901e46ef30f0e7505
Juanxiun <juanitorex1039@gmaio.com>
2025-03-20 00:43:21 -0400

    Arreglo del archivo fresh.gen

--- Macube-fresh/fresh.gen.ts
+++ Macube-fresh/fresh.gen.ts
@@ -4,7 +4,6 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $api_Middleware from "./routes/api/Middleware.ts";
 import * as $api_cliente from "./routes/api/cliente.ts";
 import * as $api_login from "./routes/api/login.tsx";
 import * as $api_session from "./routes/api/session.ts";
@@ -22,7 +21,6 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/api/Middleware.ts": $api_Middleware,
     "./routes/api/cliente.ts": $api_cliente,
     "./routes/api/login.tsx": $api_login,
     "./routes/api/session.ts": $api_session,
